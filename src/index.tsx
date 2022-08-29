2491f076e2e0133cb2c0e725546f2fd552f574b8
Khaled Muhammad <khaledmuhammad.dev@gmail.com>
2022-08-29 20:53:51 +0200

    wrap the application with MUI custom  variables and setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import '@styles/style.scss';
-import App from './views/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@styles/style.scss";
+import App from "./views/App";
+import { CustomStyle } from "./components/CustomStyle";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <CustomStyle>
+      <App />
+    </CustomStyle>
   </React.StrictMode>
 );
