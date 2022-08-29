6a2193c699df229188fc3743d2c8075bf570a01b
Khaled Muhammad <khaledmuhammad.dev@gmail.com>
2022-08-29 19:24:51 +0200

    add module augmentation for css modules

--- src/css-modules.d.ts
+++ src/css-modules.d.ts
@@ -1,3 +1,8 @@
+declare module "*.css" {
+    const styles: { [className: string]: string };
+    export default styles;
+}
+
 declare module "*.scss" {
     const styles: { [className: string]: string };
     export default styles;
