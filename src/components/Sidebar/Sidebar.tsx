4a8d9ad4f4367c281c919c5d2ee1d565752a989a
Khaled Muhammad <khaledmuhammad.dev@gmail.com>
2022-08-29 20:58:56 +0200

    build sidebar UI and add Navlinks component to it

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Stack, useTheme, IconButton } from "@mui/material";
+// Components ---
+import { Button, TextInput } from "@components/inputs";
+import { Navlinks } from "@/components/Sidebar/Navlinks";
+import BodyPortal from "@components/BodyPortal";
+import { Overlay } from "@components/Overlay";
+import styles from "./Sidebar.module.scss";
+// Icons ------
+import NoteAddIcon from "@mui/icons-material/NoteAdd";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+
+interface ISidebar {
+  matchQuery: boolean;
+  handleToggle: (val?: boolean) => void;
+}
+
+export const Sidebar = React.forwardRef<HTMLDivElement, ISidebar>(
+  ({ matchQuery, handleToggle }, ref) => {
+    const { wrapper, container } = styles;
+    const theme = useTheme();
+
+    return (
+      <div className={wrapper} ref={ref}>
+        <div className={container}>
+          <Stack
+            color={theme.customColors.textColor}
+            alignItems="center"
+            direction="row"
+          >
+            <h1 style={{ textTransform: "uppercase" }}>Logo</h1>
+            <IconButton
+              aria-label="Dark mode toggle"
+              sx={{
+                display: "flex",
+                marginLeft: "auto",
+                color: theme.customColors.textColor,
+              }}
+            >
+              <WbSunnyIcon role="img" aria-hidden="true" />
+            </IconButton>
+          </Stack>
+
+          <Stack my={4} className="search-new_note">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+              }}
+            >
+              <TextInput aria-label="Search" />
+            </form>
+
+            <Button color="info" startIcon={<NoteAddIcon />}>
+              Add Note
+            </Button>
+          </Stack>
+          <Navlinks />
+        </div>
+
+        {!matchQuery && (
+          <BodyPortal>
+            <Overlay handleToggle={handleToggle} />
+          </BodyPortal>
+        )}
+      </div>
+    );
+  }
+);
