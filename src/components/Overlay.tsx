4dc81e17196ca8a446cc83bfa89fa2992661343d
Khaled Muhammad <khaledmuhammad.dev@gmail.com>
2022-08-29 21:01:49 +0200

    feat: build overlay component

---
+++ src/components/Overlay.tsx
@@ -0,0 +1,22 @@
+import { Box } from "@mui/system";
+import { FC } from "react";
+
+interface IOverlay {
+  handleToggle: (val?: boolean) => void;
+}
+export const Overlay: FC<IOverlay> = ({ handleToggle }) => {
+  return (
+    <Box
+      onClick={() => handleToggle(false)}
+      className="overlay"
+      sx={{
+        position: "fixed",
+        left: "0",
+        top: "0",
+        width: "100vw",
+        height: "100vh",
+        background: "hsl(0, 0%, 0%, 25%)",
+      }}
+    ></Box>
+  );
+};
