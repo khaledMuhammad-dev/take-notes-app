1ddeee12d0aa831a8126a3b9809ee608dfe0c32a
Khaled Muhammad <khaledmuhammad.dev@gmail.com>
2022-08-30 10:45:26 +0200

    test: removed handleResize for testing purpose

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import { useToggle } from "@/utils/hooks";
 import { Slide, Theme, useMediaQuery } from "@mui/material";
 import { Navbar } from "@components/Navbar/Navbar";
@@ -8,14 +8,14 @@ export const Header = () => {
   const [toggle, handleToggle] = useToggle();
   const matches = useMediaQuery((theme: Theme) => theme.breakpoints.up("lg"));
 
-  useEffect(() => {
-    const handleResize = () => handleToggle(false);
-    window.addEventListener("resize", handleResize);
+  // useEffect(() => {
+  //   const handleResize = () => handleToggle(false);
+  //   window.addEventListener("resize", handleResize);
 
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, [handleToggle]);
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // }, [handleToggle]);
 
   return (
     <header>
